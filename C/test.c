7038b9e39f816919bccfc515a8794fa98b0eac68
LiaoPengyu <ariselpy@gmail.com>
2013-04-11 23:33:12 +0800

    update org article

--- C/test.c
+++ C/test.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define Length 30
+#define SentLen 300
 struct wordOb
 {
   char word[Length];
@@ -11,83 +12,79 @@ struct wordOb
 
 struct hashOb
 {
-  long int hashnum;
   struct hashOb* next;
   struct wordOb* pto;
 };
 
-struct wordOb* wordlink = NULL;
 struct hashOb* hashlink = NULL;
 struct hashOb* hashlinkend = NULL;
 
 char a[Length], b[Length];
+char sentence[SentLen];
 int n, m;
 
-struct hashOb* isearch(long h)
+struct hashOb* isearch(char* s)
 {
   struct hashOb* tmp = hashlink;
-  while(tmp && tmp->hashnum - h){
+  while(tmp && strcmp(tmp->pto->word, s)){
     tmp = tmp->next;
   }
   return tmp;
 }
 
-long int ipow(int a, int b)
+void iclearp()
 {
-  long int ret=1;
-  for(;b;b--)
-    ret *= a;
-}
-long int hash(char * word)
-{
-  long int hashnum = 0;
-  int c, i=1;
-  while(c = *word)
-    {
-      hashnum += c * ipow(26, i);
-      word ++;
-      i ++;
+  struct hashOb *tmp, *h=hashlink;
+  while (h) {
+      tmp = h;
+      h = h->next;
+      free(tmp->pto);
+      free(tmp);
     }
-  return hashnum;
 }
-
 void init()
 {
   int i;
-  long int hashnum;
   struct hashOb *h1, *h2;
   struct wordOb *w1, *w2;
+  iclearp();
   scanf("%d %d", &n, &m);
+
   for(i = 0; i < m; i++)
     {
       w2 = (struct wordOb*)malloc(sizeof(struct wordOb));
       w2->next = NULL;
       scanf("%s %s", a, w2->word);
-      hashnum = hash(a);
-      if(hashlink && (h1 = isearch(hashnum)))
+      if(hashlink && (h1 = isearch(a)))
         {
           w1 = h1->pto;
-          w1->next = w2;
         }
       else
         {
           w1 = (struct wordOb*)malloc(sizeof(struct wordOb));
-          w1->next = w2;
           strcpy(w1->word, a);
           h1 = (struct hashOb*)malloc(sizeof(struct hashOb));
-          h1->hashnum = hashnum;
           h1->pto = w1;
-          if (!hashlink) hashlink = hashlinkend = h1;
+          h1->next = NULL;
+          if (!hashlink) {
+            hashlink = hashlinkend = h1;
+          }
           else {
             hashlinkend->next = h1;
             hashlinkend = h1;
           }
         }
-      h2 = (struct hashOb*)malloc(sizeof(struct hashOb));
-      h2->hashnum = hash(b);
-      h2->pto = w1;
-      hashlinkend->next = h2;
-      hashlinkend = h2;
+      if(h2 = isearch(w2->word)){
+        w2 = h2->pto;
+      }
+      else{
+        h2 = (struct hashOb*)malloc(sizeof(struct hashOb));
+        h2->pto = w2;
+        h2->next = NULL;
+        hashlinkend->next = h2;
+        hashlinkend = h2;
+      }
+      w1->next = w2;
     }
   getchar();
 }
@@ -97,9 +94,19 @@ solve()
   int i;
   struct hashOb *h;
   struct wordOb *w;
-  while(scanf("%s", a))
+  char* iword;
+
+  //
+  h = hashlink;
+  while(h){
+    h = h->next;
+  }
+  fgets(sentence, SentLen, stdin);
+  for (iword = strtok(sentence, " \n\r");
+       iword;
+       iword = strtok(NULL, " \n\r"))
     {
-      if(h = isearch(hash(a)))
+      if(h = isearch(iword))
         {
           for(i = n-1, w = h->pto; w->next&&i; i--)
             {
@@ -109,7 +116,7 @@ solve()
         }
       else
         {
-          printf(" %s", a);
+          printf(" %s", iword);
         }
     }
   puts("");
